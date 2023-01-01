a1a336c72e16cf6028540060219b77838bb10aab
Bohyun Kang <bbohyun@kakao.com>
2023-01-01 16:55:03 +0900

    WelcomeController 생성 및 CORS 허용
    - WelcomeController 생성
    - CORS origin 허용 및 webSecurity 비활성화

--- src/main/java/kr/bohyunkang/shimpyo/ShimpyoApplication.java
+++ src/main/java/kr/bohyunkang/shimpyo/ShimpyoApplication.java
@@ -2,12 +2,34 @@ package kr.bohyunkang.shimpyo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ShimpyoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShimpyoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShimpyoApplication.class, args);
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/**");
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
 
 }
